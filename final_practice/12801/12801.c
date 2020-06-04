a5d367986c4a1b9a933e80cbc7d66fcf8c2fca91
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-06-04 21:32:29 +0800

    12801 done

--- final_practice/12801/12801.c
+++ final_practice/12801/12801.c
@@ -94,21 +94,78 @@ void oldWay(int n, long int m, long int *coffees){
     }
 }
 
-void newWay(int n, long int m, long int *coffees, long int maxWorks){
-    int d = 1;
-    int is_done = 0;
+int binSearch(long int *seq, long int target, int start, int end){
+    // To find the largest number that smaller than target
+    int mid = (start + end) / 2;
+    int startI = start, endI = end;
+    // printf("Binary Search Target: %ld within %d ~ %d\n", target, start, end);
 
-    // Iteate Days
-    for(int d = 1; d < n; d++){
-        int isCoffeeNotWork = 0;
+    while(startI != endI){
+        // printf("Binary Search: startI:[%d]%ld mid:[%d]%ld endI:[%d]%ld", startI, seq[startI], mid, seq[mid], endI, seq[endI]);
+        if(target > seq[mid]){
+            endI = mid;
+            // printf(" T > M\n");
+        }else{
+            startI = mid + 1;
+            // printf(" T <= M\n");
+        }
+
+        mid = (startI + endI) / 2;
+    }
+
+    return mid;
+}
+
+void newWay(int n, long int m, long int *coffees, long int *prefixSum, long int maxWorks){
+    // Iterate Days
+    for(int d = 1; d <= n; d++){
         int maxRound = n / d;
-        long int accumCode = 0;
+        long int cost = 0;
+        long int accumCode = prefixSum[d];
+
+        // printf("For %d Days\n", d);
+        // printf("->Round 0: %ld\n", accumCode);
+
+        for(int r = 1; r <= maxRound; r++){
+            if(d * (r + 1) - 1 < n && coffees[d * (r + 1) - 1] == r){
+                // the last round that can add
+                cost += d * r;
+                accumCode = prefixSum[d * (r + 1)] - cost;
+                // printf("->Round %d: %ld\n", r, accumCode);
+                break;
+            }else if(d * (r + 1) - 1 < n && coffees[d * (r + 1) - 1] > r){
+                // add next round
+                cost += d * r;
+                accumCode = prefixSum[d * (r + 1)] - cost;
+                // printf("->Round %d: %ld\n", r, accumCode);
+            }else if(d * r < n){
+                
+                int binSearchEnd = n - 1 < d * (r + 1) - 1? n - 1 : d * (r + 1) - 1;
+                // printf("Search for %d ~ %d < %d\n", d * r, binSearchEnd, n - 1);
+                int searchRes = binSearch(coffees, r, d * r, binSearchEnd);
+                int range = searchRes - (d * r);
+                cost += range * r;
+                accumCode = prefixSum[searchRes] - cost;
+
+                // printf("->Tail: [%d]%ld ~ [%d]%ld(%d) = %ld In Total\n", searchRes, coffees[searchRes], (d * r), coffees[(d * r)], range, accumCode);
+                break;
+            }
+            
+
+            if(accumCode >= m){break;}
+        }
+
+        if(accumCode >= m){
+            printf("%d\n", d);
+            break;
+        }
     }
 }
 
 int main(){
     int t = 0;
     long int coffees[200005] = {0};
+    long int prefixSum[200005] = {0};
     scanf("%d\n", &t);
 
     for(int i = 0; i < t; i++){
@@ -120,6 +177,9 @@ int main(){
         for(int j = 0; j < n; j++){
             scanf("%ld", &(coffees[j]));
             maxWorks += coffees[j];
+            // Note: coffee[k] = prefixSum[k + 1] - prefixSum[k]
+            prefixSum[j + 1] = maxWorks;
+
         }
 
         if(maxWorks < m){
@@ -127,10 +187,12 @@ int main(){
 
         }else if(maxWorks == m){
             printf("%d\n", n);
-
         }else{
+            // printf("n:%d m:%ld\n", n, m);
+            newWay(n, m, coffees, prefixSum, maxWorks);
+            // printf("OldWay: ");
             // oldWay(n, m, coffees);
-
+            // printf("===========================\n");
         }
             
     }
