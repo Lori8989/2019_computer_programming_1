364c000072c540278bec9dcb77b77ff66d75acec
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-06-28 01:49:16 +0800

    12144, 11759 done

--- final_practice/12144/12144.c
+++ final_practice/12144/12144.c
@@ -35,6 +35,7 @@
 // Sample Output  
 // YOU DIED
 
+// (0, 0) may occur monsters
 #include<stdio.h>
 #include<stdlib.h>
 #define MapSize 5
@@ -43,14 +44,20 @@
 typedef struct monster{
     int hp;
     int attack;
-    int x;
-    int y;
+    int r;
+    int c;
+    int isEverPass;
 }Monster;
 
 typedef struct you{
     int hp;
     int attack;
+    int r;
+    int c;
 }You;
+
+const int dirR[4] = {1, -1, 0, 0};
+const int dirC[4] = {0, 0, 1, -1};
 
 void showMap(int (*map)[MapSize]){
     for(int i = 0; i < MapSize; i++){
@@ -61,109 +68,89 @@ void showMap(int (*map)[MapSize]){
     }
 }
 
-// int fight(const int youHp, const int youAttack, const Monster mon){
-//     if(youAttack <= 0){
-//         return Inf;
+int fight(const You you, const Monster mon){
+    if(you.attack <= 0 && mon.hp > 0){
+        return 0;
+    }else{
+        return you.hp - (((mon.hp / you.attack) + (mon.hp % you.attack > 0) - 1) * mon.attack);
+        // return you - ((mon.hp / youAttack + 1) * mon.attack);
+    }
+}
+
+// int fight(const You you, const Monster mon){
+//     // Return HP of hero after fight with monster
+//     if(you.attack <= 0 && mon.hp > 0){
+//         return 0;
 //     }else{
-//         return youHp - (((mon.hp / youAttack) + (mon.hp % youAttack > 0) - 1) * mon.attack);
-//         // return youHp - ((mon.hp / youAttack + 1) * mon.attack);
+//         int tempHp = you.hp;
+//         int tempMonHp = mon.hp;
+//         while(tempMonHp > 0){
+//             tempMonHp -= you.attack;
+//             if(tempMonHp > 0){
+//                 tempHp -= mon.attack;
+//             }
+//         }
+//         return tempHp;
 //     }
 // }
 
-int fight(const int youHp, const int youAttack, const Monster mon){
-    if(youAttack <= 0){
-        return Inf;
-    }else{
-        int tempHp = youHp;
-        int tempMonHp = mon.hp;
-        while(tempMonHp > 0){
-            tempMonHp -= youAttack;
-            if(tempMonHp > 0){
-                tempHp -= mon.attack;
+int dfs(Monster (*mons)[MapSize], const You you){
+    int isAlive = 0;
+    You nowYou = you;
+    if(nowYou.r >= 0 && nowYou.c >= 0 && nowYou.r < MapSize && nowYou.c < MapSize){
+        if(!mons[nowYou.r][nowYou.c].isEverPass){
+            nowYou.hp = fight(nowYou, mons[nowYou.r][nowYou.c]);
+            if(nowYou.hp > 0){
+                if(nowYou.r == 4 && nowYou.c == 4) {return 1;}
+
+                mons[nowYou.r][nowYou.c].isEverPass = 1;
+                // printf("you: HP: %d, ATK: %d, (%d, %d)\n", nowYou.hp, nowYou.attack, nowYou.r, nowYou.c);
+                for(int i = 0; i < 4; i++){
+                    You nextY = nowYou;
+                    nextY.r += dirR[i];
+                    nextY.c += dirC[i];
+                    isAlive = dfs(mons, nextY);
+                    if(isAlive){return 1;}
+                }
+                mons[nowYou.r][nowYou.c].isEverPass = 0;
             }
         }
-        // return youHp - (((mon.hp / youAttack) + (mon.hp % youAttack > 0) - 1) * mon.attack);
-        // return youHp - ((mon.hp / youAttack + 1) * mon.attack);
-        return tempHp;
     }
+    return 0;
 }
 
 int main(){
     You y;
     Monster mons[MapSize][MapSize];
-    int maxHpMap[MapSize][MapSize] = {0};
-    int k = 0;
+    int k = 0, isAlive = 0;
 
     scanf("%d %d %d\n", &(y.hp), &(y.attack), &k);
-    maxHpMap[0][0] = y.hp;
+    y.r = 0;
+    y.c = 0;
 
     for(int i = 0; i < MapSize; i++){
         for(int j = 0; j < MapSize; j++){
             mons[i][j].hp = 0;
             mons[i][j].attack = 0;
+            mons[i][j].isEverPass = 0;
         }
     }
 
     for(int i = 0; i < k; i++){
-        int hp = 0, attack = 0, x = 0, y = 0;
-        scanf("%d %d %d %d\n", &hp, &attack, &x, &y);
-        mons[x - 1][y - 1].hp = hp;
-        mons[x - 1][y - 1].attack = attack;
-        mons[x - 1][y - 1].x = x - 1;
-        mons[x - 1][y - 1].y = y - 1;
+        int hp = 0, attack = 0, r = 0, c = 0;
+        scanf("%d %d %d %d\n", &hp, &attack, &r, &c);
+        mons[r - 1][c - 1].hp = hp;
+        mons[r - 1][c - 1].attack = attack;
+        mons[r - 1][c - 1].r = r - 1;
+        mons[r - 1][c - 1].c = c - 1;
     }
+    // mons[0][0].isEverPass = 1;
     
     // printf("Monster HP: %d Attack: %d\n", mons[2][1].hp, mons[2][1].attack);
+    isAlive = dfs(mons, y);
+    // showMap(maxHpMap);
 
-    for(int k = 0; k < 25; k++){
-        for(int i = 0; i < MapSize; i++){
-            for(int j = 0; j < MapSize; j++){
-                if(maxHpMap[i][j] > 0){
-                    // printf("Iterate K: %d, [%d][%d]\n", k, i, j);
-                    // printf("Hero HP:%d Attack:%d\n", maxHpMap[i][j], y.attack);
-                    if(i + 1 < MapSize){
-                        int fightRes = fight(maxHpMap[i][j], y.attack, mons[i + 1][j]);
-                        // fightRes = fightRes < 0? fightRes : fightRes;
-                        // printf("Monster[%d][%d] HP: %d Attack: %d Res: %d\n", i + 1, j, mons[i + 1][j].hp, mons[i + 1][j].attack, fightRes);
-                        if(fightRes > maxHpMap[i + 1][j]){
-                            maxHpMap[i + 1][j] = fightRes;
-                        }
-                    }
-
-                    if(i - 1 >= 0){
-                        int fightRes = fight(maxHpMap[i][j], y.attack, mons[i - 1][j]);
-                        // fightRes = fightRes < 0? fightRes : fightRes;
-                        // printf("Monster[%d][%d] HP: %d Attack: %d Res: %d\n", i - 1, j, mons[i - 1][j].hp, mons[i - 1][j].attack, fightRes);
-                        if(fightRes > maxHpMap[i - 1][j]){
-                            maxHpMap[i - 1][j] = fightRes;
-                        }
-                    }
-
-                    if(j + 1 < MapSize){
-                        int fightRes = fight(maxHpMap[i][j], y.attack, mons[i][j + 1]);
-                        // fightRes = fightRes < 0? fightRes : fightRes;
-                        // printf("Monster[%d][%d] HP: %d Attack: %d Res: %d\n", i, j + 1, mons[i][j + 1].hp, mons[i][j + 1].attack, fightRes);
-                        if(fightRes > maxHpMap[i][j + 1]){
-                            maxHpMap[i][j + 1] = fightRes;
-                        }
-                    }
-                    
-                    if(j - 1 >= 0){
-                        int fightRes = fight(maxHpMap[i][j], y.attack, mons[i][j - 1]);
-                        // fightRes = fightRes < 0? fightRes : fightRes;
-                        // printf("Monster[%d][%d] HP: %d Attack: %d Res: %d\n", i, j - 1, mons[i][j - 1].hp, mons[i][j - 1].attack, fightRes);
-                        if(fightRes > maxHpMap[i][j - 1]){
-                            maxHpMap[i][j - 1] = fightRes;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    showMap(maxHpMap);
-
-    if(maxHpMap[4][4] > 0){printf("HEIR OF FIRE DESTROYED\n");}
+    if(isAlive){printf("HEIR OF FIRE DESTROYED\n");}
     else{printf("YOU DIED\n");}
 
     return 0;
--- readme.md
+++ readme.md
@@ -109,7 +109,7 @@ Final Exam
 
 > [Week14 12141 Ugandan Knuckles's code](./final_practice/12141/12141.c) Done
 
-> [Week14 12144 Dark Souls](./final_practice/12144/12144.c)
+> [Week14 12144 Dark Souls](./final_practice/12144/12144.c) Done
 
 > [Week14 12305 Airplane Shooter](./final_practice/12305/12305.c) Done
 
@@ -121,7 +121,7 @@ Final Exam
 
 > [Week15 12146 Too Many Things to Buy](./final_practice/12146/12146.c) Done
 
-> [11759 B - Great Depression Again](./final_practice/11759/11759.c)
+> [11759 B - Great Depression Again](./final_practice/11759/11759.c) Done
 
 > [Week16 12241 Restaurants in Hsinchu](./final_practice/12241/12241.c) Done
 
